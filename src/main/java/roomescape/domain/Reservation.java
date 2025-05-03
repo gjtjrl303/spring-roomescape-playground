e09efa93f058008eb1868b8450ffee631726f9d2
gjtjrl303 <gjtjrl303@naver.com>
2025-05-03 10:59:18 +0900

    refactor: Reservation 클래스의 필드 final 제거하여 유연한 값 변경 가능하게 수정

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -5,16 +5,27 @@ import java.time.LocalTime;
 
 public class Reservation {
 
-    private final Long id;
-    private final String name;
-    private final LocalDate date;
-    private final LocalTime time;
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
 
     public Reservation(Long id, String name, LocalDate date, LocalTime time) {
         this.id = id;
         this.name = name;
         this.date = date;
         this.time = time;
+    }
+
+    public Reservation(String name, LocalDate date, LocalTime time) {
+        this.id = null;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation withId(Long id) {
+        return new Reservation(id, this.name, this.date, this.time);
     }
 
     public Long getId() {
