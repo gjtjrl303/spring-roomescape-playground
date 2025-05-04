18331a3db0bce8d595740b365928f01322b65b94
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-05-04 15:04:15 +0900

    고생하셨습니다.
    :tada: PR 머지 완료! :tada:

---
+++ src/main/java/roomescape/domain/Reservation.java
@@ -0,0 +1,46 @@
+package roomescape.domain;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Reservation {
+
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Reservation(Long id, String name, LocalDate date, LocalTime time) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation(String name, LocalDate date, LocalTime time) {
+        this.id = null;
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Reservation withId(Long id) {
+        return new Reservation(id, this.name, this.date, this.time);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+}
