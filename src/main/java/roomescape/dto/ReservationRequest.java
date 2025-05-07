6f5afca9e20891d645068c8c452a199e07280929
gjtjrl303 <gjtjrl303@naver.com>
2025-05-07 19:48:53 +0900

    feat: ReservationRequest dto 구현

---
+++ src/main/java/roomescape/dto/ReservationRequest.java
@@ -0,0 +1,16 @@
+package roomescape.dto;
+
+import roomescape.domain.Reservation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ReservationRequest(
+        String name,
+        LocalDate date,
+        LocalTime time
+) {
+    public Reservation toEntity() {
+        return new Reservation(name, date, time);
+    }
+}
