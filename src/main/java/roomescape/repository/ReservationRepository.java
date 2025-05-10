fa64bd38313e6cba8a7882413254a6396028bfd9
gjtjrl303 <gjtjrl303@naver.com>
2025-05-10 13:42:30 +0900

    refactor: 존재하는 id 추가할때 커스텀 예외를 던지도록 변경

--- src/main/java/roomescape/repository/ReservationRepository.java
+++ src/main/java/roomescape/repository/ReservationRepository.java
@@ -2,7 +2,9 @@ package roomescape.repository;
 
 import org.springframework.stereotype.Repository;
 import roomescape.domain.Reservation;
+import roomescape.exception.InvalidReservationDateException;
 import roomescape.exception.NotFoundReservationException;
+import roomescape.exception.ReservationAlreadyExistsException;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -10,20 +12,20 @@ import java.util.concurrent.atomic.AtomicLong;
 @Repository
 public class ReservationRepository {
 
-    private static Map<Long, Reservation> store = new LinkedHashMap<>();
+    private static Map<Long, Reservation> store = new HashMap<>();
 
     private AtomicLong index = new AtomicLong(0);
 
     public Reservation save(Reservation reservation) {
         if (reservation.getId() != null) {
-            throw new IllegalStateException("이미 ID가 존재하는 예약은 저장할 수 없습니다.");
+            throw new ReservationAlreadyExistsException("이미 ID가 존재하는 예약은 저장할 수 없습니다.");
         }
         Reservation saved = reservation.withId(index.incrementAndGet());
         store.put(saved.getId(), saved);
         return saved;
     }
 
-    public List<Reservation> findAll() {
+    public List<Reservation>    findAll() {
         return new ArrayList<>(store.values());
     }
 
