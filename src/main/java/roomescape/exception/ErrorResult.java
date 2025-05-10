809e2db51e4d98ecc4462f0b66dcaccfd37e0958
gjtjrl303 <gjtjrl303@naver.com>
2025-05-10 12:56:59 +0900

    refactor: ErrorResult class에서 record로 변경

--- src/main/java/roomescape/exception/ErrorResult.java
+++ src/main/java/roomescape/exception/ErrorResult.java
@@ -1,20 +1,6 @@
 package roomescape.exception;
 
-public class ErrorResult {
-
-    private String code;
-    private String message;
-
-    public ErrorResult(String code, String message) {
-        this.code = code;
-        this.message = message;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-}
+public record ErrorResult(
+        String code,
+        String message
+) {}
