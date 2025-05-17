5d3f33617465130062230ddec72bee0fb48f2fed
gjtjrl303 <gjtjrl303@naver.com>
2025-05-17 18:23:07 +0900

    refactor: reservationDAO 클래스명 reservationDao로 변경

--- src/main/java/roomescape/service/ReservationService.java
+++ src/main/java/roomescape/service/ReservationService.java
@@ -1,9 +1,9 @@
 package roomescape.service;
 
 import org.springframework.stereotype.Service;
-import roomescape.dao.ReservationDAO;
+import roomescape.dao.ReservationDao;
 import roomescape.domain.Reservation;
-import roomescape.service.dto.ReservationCommand;
+import roomescape.service.dto.ReservationSaveCommand;
 import roomescape.service.dto.ReservationResult;
 
 import java.util.List;
@@ -11,26 +11,26 @@ import java.util.List;
 @Service
 public class ReservationService {
 
-    private final ReservationDAO reservationDAO;
+    private final ReservationDao reservationDao;
 
-    public ReservationService(ReservationDAO reservationDAO) {
-        this.reservationDAO = reservationDAO;
+    public ReservationService(ReservationDao reservationDAO) {
+        this.reservationDao = reservationDAO;
     }
 
-    public ReservationResult save(ReservationCommand command) {
+    public ReservationResult save(ReservationSaveCommand command) {
         Reservation reservation = command.toEntity();
-        Reservation savedReservation = reservationDAO.save(reservation);
+        Reservation savedReservation = reservationDao.save(reservation);
         return ReservationResult.from(savedReservation);
     }
 
     public List<ReservationResult> findAll() {
-        List<Reservation> reservations = reservationDAO.findAll();
+        List<Reservation> reservations = reservationDao.findAll();
         return reservations.stream()
                 .map(ReservationResult::from)
                 .toList();
     }
 
     public void delete(Long id) {
-        reservationDAO.delete(id);
+        reservationDao.delete(id);
     }
 }
