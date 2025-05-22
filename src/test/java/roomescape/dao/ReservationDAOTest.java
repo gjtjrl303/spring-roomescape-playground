3e85572cce331bc8512335bd7b839d5f15fe8506
gjtjrl303 <gjtjrl303@naver.com>
2025-05-22 20:33:29 +0900

    refactor: 테스트 코드 작성

--- src/test/java/roomescape/dao/ReservationDAOTest.java
+++ src/test/java/roomescape/dao/ReservationDAOTest.java
@@ -3,9 +3,10 @@ package roomescape.dao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import roomescape.domain.Reservation;
+import roomescape.domain.Time;
 import roomescape.exception.NotFoundReservationException;
-import roomescape.service.dto.ReservationSaveCommand;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
@@ -16,36 +17,43 @@ import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationDAOTest {
 
     @Autowired
     ReservationDao reservationDao;
-    @Autowired DataSource dataSource;
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    TimeDao timeDao;
 
     @Test
     void connection() {
-        try (final var connection = dataSource.getConnection()){
+        try (final var connection = dataSource.getConnection()) {
             assertThat(connection).isNotNull();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
     void save() {
-        Reservation reservation = new Reservation( "name", LocalDate.parse("2026-05-02"), LocalTime.parse("12:00"));
+        Time time = new Time(LocalTime.parse("10:00"));
+        Time savedTime = timeDao.save(time);
+        Reservation reservation = new Reservation("name", LocalDate.parse("2026-05-02"), savedTime);
         Reservation savedReservation = reservationDao.save(reservation);
 
+        assertThat(savedReservation.getId()).isEqualTo(1L);
         assertThat(savedReservation.getName()).isEqualTo("name");
         assertThat(savedReservation.getDate()).isEqualTo(LocalDate.parse("2026-05-02"));
-        assertThat(savedReservation.getTime()).isEqualTo(LocalTime.parse("12:00"));
+        assertThat(savedReservation.getTime().getId()).isEqualTo(savedTime.getId());
     }
 
     @Test
     void findById() {
-        //given
-        Reservation reservation = new Reservation("name", LocalDate.parse("2026-05-02"), LocalTime.parse("12:00"));
+        Time time = new Time(LocalTime.parse("10:00"));
+        Time savedTime = timeDao.save(time);
+        Reservation reservation = new Reservation("name", LocalDate.parse("2026-05-02"), savedTime);
         Reservation savedReservation = reservationDao.save(reservation);
 
         //when
@@ -55,30 +63,38 @@ class ReservationDAOTest {
         assertThat(findReservation.getId()).isEqualTo(savedReservation.getId());
         assertThat(findReservation.getName()).isEqualTo(savedReservation.getName());
         assertThat(findReservation.getDate()).isEqualTo(savedReservation.getDate());
-        assertThat(findReservation.getTime()).isEqualTo(savedReservation.getTime());
+        assertThat(findReservation.getTime().getId()).isEqualTo(savedReservation.getTime().getId());
     }
 
     @Test
     void update() {
         //given
-        Reservation reservation = new Reservation( "name", LocalDate.parse("2026-05-02"), LocalTime.parse("12:00"));
+        Time time1 = new Time(LocalTime.parse("10:00"));
+        Time time2 = new Time(LocalTime.parse("11:00"));
+        Time savedTime1 = timeDao.save(time1);
+        Time savedTime2  = timeDao.save(time2);
+
+        Reservation reservation = new Reservation("name", LocalDate.parse("2026-05-02"), savedTime1);
         Reservation savedReservation = reservationDao.save(reservation);
-        ReservationSaveCommand reservationCommand = new ReservationSaveCommand("name1", LocalDate.parse("2026-05-03"), LocalTime.parse("13:00"));
 
         //when
-        reservationDao.update(savedReservation.getId(), reservationCommand.toEntity());
+        Reservation updateReservationParam = new Reservation("newName", LocalDate.parse("2026-05-03"), savedTime2);
+        reservationDao.update(savedReservation.getId(), updateReservationParam);
         Reservation updatedReservation = reservationDao.findById(savedReservation.getId());
+
         //then
         assertThat(updatedReservation.getId()).isEqualTo(savedReservation.getId());
-        assertThat(updatedReservation.getName()).isEqualTo(reservationCommand.name());
-        assertThat(updatedReservation.getDate()).isEqualTo(reservationCommand.date());
-        assertThat(updatedReservation.getTime()).isEqualTo(reservationCommand.time());
+        assertThat(updatedReservation.getName()).isEqualTo("newName");
+        assertThat(updatedReservation.getDate()).isEqualTo("2026-05-03");
+        assertThat(updatedReservation.getTime().getId()).isEqualTo(savedTime2.getId());
     }
 
     @Test
     void delete() {
         //given
-        Reservation reservation = new Reservation( "name", LocalDate.parse("2026-05-02"), LocalTime.parse("12:00"));
+        Time time = new Time(LocalTime.parse("10:00"));
+        Time savedTime = timeDao.save(time);
+        Reservation reservation = new Reservation("name", LocalDate.parse("2026-05-02"), savedTime);
         Reservation savedReservation = reservationDao.save(reservation);
 
         //when
