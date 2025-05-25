ab7bb9c87263c31ca615bfd76624479aa64e6ea5
gjtjrl303 <gjtjrl303@naver.com>
2025-05-25 11:58:36 +0900

    feat: 커스텀 예외의 부모 클래스 RoomEscapeException 구현

--- src/main/java/roomescape/exception/RoomEscapeException.java
+++ src/main/java/roomescape/exception/RoomEscapeException.java
@@ -1,7 +1,27 @@
 package roomescape.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class RoomEscapeException extends RuntimeException {
-  public RoomEscapeException(String message) {
+
+  private final String errorCode;
+  private final HttpStatus httpStatus;
+
+  protected RoomEscapeException(String message) {
+    this(message, "ROOM_ESCAPE_ERROR", HttpStatus.BAD_REQUEST);
+  }
+
+  public RoomEscapeException(String message, String errorCode, HttpStatus httpStatus) {
     super(message);
+    this.errorCode = errorCode;
+    this.httpStatus = httpStatus;
+  }
+
+  public String getErrorCode() {
+    return errorCode;
+  }
+
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
   }
 }
